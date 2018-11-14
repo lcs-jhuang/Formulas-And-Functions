20816f4df032043cbd0eac9d0565be780c2539c9
Joe Huang <jhuang@lcs.on.ca>
2018-11-14 14:25:49 -0500

    Formula

--- Formulas-And-Functions/main.swift
+++ Formulas-And-Functions/main.swift
@@ -12,3 +12,33 @@ import Foundation
 let a = 7.0
 let areaOfSquare = forSquareWith(sideLength: a)
 print("The area of a square with side length \(a) is \(areaOfSquare)")
+
+// Calculate the area of a rectangle
+let m = 10.0
+let n = 2.0
+let areaOfRectangle = forRectangleWith(length: m, width: n)
+print(areaOfRectangle)
+
+
+
+// HOMEWORK
+
+// Caluculate the area of a paralleloram
+let b = 4.0
+let h = 5.0
+let areaOfParalleelogram = forParallelogramWith(base: b, height: h)
+print("Area of parallelogram is: \(areaOfParalleelogram)")
+
+// Calculate the AREA of a trapezoid
+let b1 = 2.0
+let b2 = 3.0
+let areaOfTrapezoid = forTrapezoidWith(base: b1, base: b2, height: h)
+print("Area of trapezoid is: \(areaOfTrapezoid)")
+
+// Calculate the SLOPE of a line
+let x = 2.0
+let x2 = 8.0
+let y = 1.0
+let y2 = 5.0
+let slopeOfALine = forSlopeWith(x: x, x2: x2, y: y, y2: y2)
+print("Slope of a line is: \(slopeOfALine)")
